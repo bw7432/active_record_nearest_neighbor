b47e13248ed4f24f2e3ef624d6ecdb81eeae1d26
Ben Warren <benwade24@gmail.com>
2019-11-26 23:33:15 -0500

    adapter

--- lib/active_record_nearest_neighbor/railtie.rb
+++ lib/active_record_nearest_neighbor/railtie.rb
@@ -6,7 +6,8 @@ module ActiveRecordNearestNeighbor
 
     rake_tasks do
       load 'tasks/active_record_nearest_neighbor_tasks.rake'
-      load 'active_record/connection_adapters/postgis_adapter/databases.rake'
+      load "active_record/connection_adapters/postgis/postgis_database_tasks"
+      # load 'active_record/connection_adapters/postgis_adapter/databases.rake'
     end
 
   end
